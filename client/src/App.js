9fd71e24d51b2a2cf7db64fb07ac7a811dd9cb46
jinstrider2000 <efrainperezjr@live.com>
2018-10-02 23:57:00 -0400

    added nav bar file, started routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <NavBar/>
+        <br/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/actors" component={Actors}/>
+        <Route exact path="/directors" component={Directors}/>
+        <Route exact path="/movies" component={Movies}/>
+      </React.Fragment>
+    </BrowserRouter>
+  );
+};
 
 export default App;
