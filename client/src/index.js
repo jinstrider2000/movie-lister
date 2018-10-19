cd1f8ebbb6e75fbc476e1faf34b3385202aa083e
jinstrider2000 <efrainperezjr@live.com>
2018-10-19 14:33:44 -0400

    updated more stuff, added some reducer files

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 import App from './components/App';
+import { Provider } from "react-redux";
+import reducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer,applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
