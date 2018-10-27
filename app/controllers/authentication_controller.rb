7bef368fb0e5fe8d38fc6e6fb747c3db21cefb27
jinstrider2000 <efrainperezjr@live.com>
2018-10-27 13:55:19 -0400

    updated controllers

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,12 @@
 class AuthenticationController < ApplicationController
 
   def authenticate
-    
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      render json: {user_id: @user.id}, status: 200
+    else
+      render json: {user_id: nil}, status: 401
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.persisted?
-      render json: {message: t(".success_msg"), user_id: @user.id}, status: 201
+      render json: {user_id: @user.id}, status: 201
     else
-      render json: {message: t(".failure_msg")}, status: 404
+      render json: {user_id: nil}, status: 404
     end
   end
 
