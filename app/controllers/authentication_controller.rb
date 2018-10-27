80dd9ce609ee867a7fc6bfc94f59d7f334b2b466
jinstrider2000 <efrainperezjr@live.com>
2018-10-26 22:31:00 -0400

    routes updated

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,7 @@
+class AuthenticationController < ApplicationController
+
+  def authenticate
+    
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
-  
-  respond_to :json
 
   def create
-    
+    @user = User.create(user_params)
+    if @user.persisted?
+      render json: {message: t(".success_msg"), user_id: @user.id}, status: 201
+    else
+      render json: {message: t(".failure_msg")}, status: 404
+    end
+  end
+
+  private
+
+  def user_params
+      params.permit(:username, :password)
   end
 
 end
