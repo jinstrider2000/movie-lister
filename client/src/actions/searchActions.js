eda8567ec76cdfe701312af1eb8e672e9bee4acc
jinstrider2000 <efrainperezjr@live.com>
2018-11-07 10:55:40 -0500

    start search actions

--- client/src/actions/searchActions.js
+++ client/src/actions/searchActions.js
@@ -1,1 +1,10 @@
-export const loadResults = ({})  
+const imdb = require('imdb-api');
+const client = new imdb.Client({apiKey: "d7196e79"});
+
+export const loadSearchResults = (searchTerm) => {
+  return (
+    (dispatch, getState) => {
+      
+    }
+  )
+}  
