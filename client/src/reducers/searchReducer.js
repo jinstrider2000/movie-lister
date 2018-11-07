aae47d6ffbac810c94c72da553a403d865b2a2f0
jinstrider2000 <efrainperezjr@live.com>
2018-11-06 23:28:08 -0500

    fixing search reducer

--- client/src/reducers/searchReducer.js
+++ client/src/reducers/searchReducer.js
@@ -1,11 +1,15 @@
-export default (state={currentSearchObj: undefined, results: [], totalResults: undefined, totalLoaded: undefined}, action) => {
+export default (state={currentSearchObj: undefined, results: [], totalResults: undefined, totalLoaded: undefined, errorMessage: undefined}, action) => {
   switch (action.type) {
     case "LOAD_RESULTS":
-      return {...state};
+      if (!state.currentSearchObj) {
+        return {...state, currentSearchObj: action.currentSearchObj, results: action.results, totalResults: action.totalResults, totalLoaded: action.results.length}
+      } else {
+        return {...state, }
+      }
     case "LOAD_MOVIE":
       return 
     case "SIGN_OUT":
-      return {currentSearchObj: undefined, results: [], totalResults: undefined, totalLoaded: undefined};
+      return {currentSearchObj: undefined, results: [], totalResults: undefined, totalLoaded: undefined, errorMessage: undefined};
     default:
       return state;
   }
