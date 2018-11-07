76ad08914ee12cc96ad1dc994ffb535c9c03cad9
jinstrider2000 <efrainperezjr@live.com>
2018-11-07 00:48:16 -0500

    did search reducer

--- client/src/reducers/searchReducer.js
+++ client/src/reducers/searchReducer.js
@@ -1,15 +1,16 @@
-export default (state={currentSearchObj: undefined, results: [], totalResults: undefined, totalLoaded: undefined, errorMessage: undefined}, action) => {
+export default (state={currentSearchObj: undefined, searchTerm: "", results: [], totalResults: 0, totalLoaded: 0, error: false, errorMessage: ""}, action) => {
   switch (action.type) {
-    case "LOAD_RESULTS":
-      if (!state.currentSearchObj) {
-        return {...state, currentSearchObj: action.currentSearchObj, results: action.results, totalResults: action.totalResults, totalLoaded: action.results.length}
+    case "LOAD_SEARCH_RESULTS":
+      if (!state.currentSearchObj || action.searchTerm !== state.searchTerm) {
+        return {...state, currentSearchObj: action.currentSearchObj, searchTerm: action.searchTerm, results: action.results, totalResults: action.totalResults, totalLoaded: action.results.length}
       } else {
-        return {...state, }
+        return {...state, currentSearchObj: action.currentSearchObj, results: [...state.results, ...action.results], totalLoaded: state.totalLoaded + action.results.length}
       }
+    case "DISPLAY_SEARCH_ERROR":
+      return {currentSearchObj: undefined, searchTerm: "", results: [], totalResults: 0, totalLoaded: 0, error: true, errorMessage: action.message}
     case "LOAD_MOVIE":
-      return 
     case "SIGN_OUT":
-      return {currentSearchObj: undefined, results: [], totalResults: undefined, totalLoaded: undefined, errorMessage: undefined};
+      return {currentSearchObj: undefined, searchTerm: "", results: [], totalResults: 0, totalLoaded: 0, error: false, errorMessage: ""};
     default:
       return state;
   }
