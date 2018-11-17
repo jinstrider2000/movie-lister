3dfc5e30dfad0399973f16eff7b2887543870eb6
jinstrider2000 <efrainperezjr@live.com>
2018-11-17 08:10:08 -0500

    conditional information presentation

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -26,32 +26,22 @@ class Movie extends Component {
     } else {
       output = 
         <div id="movie-details" className="main-abs-ps movie-slide-in-container">
+          {console.log(this.state.info)}
           <Transparency id="transparency-search-results" condition={this.props.searchActivity || this.props.searchError} classes="background-fade"/>
           <Transparency id="transparency-movie-info" condition={true} classes="movie-slide-in-transparency"/>
           <img id="movie-poster" src={this.state.info.poster !== "N/A" ? this.state.info.poster : posterPlaceHolder} alt="Movie Poster"/>
           <h1>{this.state.info.title}</h1>
           {this.state.info.production !== "N/A" ? <h2>(A {this.state.info.production} Release)</h2> : null }
-          <h3>Directed by</h3>
-          <h4>{this.state.info.director}</h4>
-          <h3>Written by</h3>
-          <h4>{this.state.info.writer}</h4>
-          <h3>Starring</h3>
-          <h4>{this.state.info.actors}</h4>
-          <h3>Synopsis</h3>
-          <h4>{this.state.info.plot}</h4>
-          <h3>MPAA Rating</h3>
-          <h4>{this.state.info.rated}</h4>
-          <h3>Genre(s)</h3>
-          <h4>{this.state.info.genres}</h4>
-          <h3>Running Time</h3>
-          <h4>{this.state.info.runtime}</h4>
-          <h3>Released</h3>
-          <h4>{typeof(this.state.info.released) === "string" ? this.state.info.released : this.state.info.released.toLocaleDateString() }</h4>
-          <h3>Awards</h3>
-          <h4>{this.state.info.awards}</h4>
-          <img id="metacritic-logo" src={metaCriticImg} alt="Metacritic Logo"/>
-          <h3>Metacritic Score</h3>
-          <h4>{this.state.info.metascore}</h4>
+          {this.state.info.director !== "N/A" ? <React.Fragment><h3>Directed by</h3><h4>{this.state.info.director}</h4></React.Fragment> : null}
+          {this.state.info.writer !== "N/A" ? <React.Fragment><h3>Written by</h3><h4>{this.state.info.writer}</h4></React.Fragment> : null}
+          {this.state.info.actors !== "N/A" ? <React.Fragment><h3>Starring</h3><h4>{this.state.info.actors}</h4></React.Fragment> : null}
+          {this.state.info.plot !== "N/A" ? <React.Fragment><h3>Synopsis</h3><h4>{this.state.info.plot}</h4></React.Fragment> : null}
+          {this.state.info.rated !== "N/A" ? <React.Fragment><h3>MPAA Rating</h3><h4>{this.state.info.rated}</h4></React.Fragment> : null}
+          {this.state.info.genres !== "N/A" ? <React.Fragment><h3>Genre(s)</h3><h4>{this.state.info.genres}</h4></React.Fragment> : null}
+          {this.state.info.runtime !== "N/A" ? <React.Fragment><h3>Running Time</h3><h4>{this.state.info.runtime}</h4></React.Fragment> : null}
+          {this.state.info.released !== "N/A" ? <React.Fragment><h3>Released</h3><h4>{typeof(this.state.info.released) === "string" ? this.state.info.released : this.state.info.released.toLocaleDateString()}</h4></React.Fragment> : null}
+          {this.state.info.awards !== "N/A" ? <React.Fragment><h3>Awards</h3><h4>{this.state.info.awards}</h4></React.Fragment> : null}
+          {this.state.info.metascore !== "N/A" ? <React.Fragment><img id="metacritic-logo" src={metaCriticImg} alt="Metacritic Logo"/><h3>Metacritic Score</h3><h4>{this.state.info.metascore}</h4></React.Fragment> : null}
       </div>
     }
     return output;
