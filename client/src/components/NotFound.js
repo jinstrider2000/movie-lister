8f3a17fa26ea8f8fdc153b0596daf7d4a5ca76a0
jinstrider2000 <efrainperezjr@live.com>
2018-11-22 12:36:13 -0500

    added not found page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const NotFound = (props) => (<div>NotFound</div>)
+const NotFound = (props) => (
+  <div className={props.searchHappening() ? "main-abs-ps background-fade" : null}>
+    <h1 className="error-message">404 NOT FOUND</h1>
+    <br/>
+    <img src="https://i.imgur.com/88Pjm53.jpg" alt="cool hand luke quote: failure to communicate"/>
+    <br/>
+    <h2>You can go back <Link to="/">home</Link> or check out this <Link to="/movie/tt0061512">here</Link></h2>
+  </div>
+)
 
 export default NotFound
