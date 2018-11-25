fad0e4e3bd6b712bab8946f2b322192442554a92
jinstrider2000 <efrainperezjr@live.com>
2018-11-24 19:43:07 -0500

    updated css classes for generality

--- client/src/components/SearchResultItem.js
+++ client/src/components/SearchResultItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 import posterPlaceHolder from '../assets/images/film-poster-placeholder.png';
 
 const SearchResultItem = ({info}) => (
-  <div className="search-result-item search-result-item-slide-in">
-    {info.poster !== "N/A" ? <img src={info.poster} className="search-result-item-poster" alt="search result poster"/>: <img src={posterPlaceHolder} className="search-result-item-poster" alt="search result poster"/>}
-    <h3 className="search-result-info">{info.title}</h3>
-    <h4 className="search-result-info">{typeof(info.year) === "number" ? info.year.toString() : null}</h4>
+  <div className="movie-preview movie-preview-slide-in">
+    {info.poster !== "N/A" ? <img src={info.poster} className="movie-preview-poster" alt="search result poster"/>: <img src={posterPlaceHolder} className="movie-preview-poster" alt="search result poster"/>}
+    <h3 className="movie-preview-info">{info.title}</h3>
+    <h4 className="movie-preview-info">{typeof(info.year) === "number" ? info.year.toString() : null}</h4>
   </div>
 )
 
--- client/src/components/SearchResultList.js
+++ client/src/components/SearchResultList.js
@@ -4,7 +4,7 @@ import SearchResultItem from './SearchResultItem';
 
 const SearchResultList = ({searchTerm,results,handleResultClick}) => (
   <React.Fragment>
-    {results.map((result, index) => <div className="row" key={`${index}-${searchTerm}`} ><Link to={`/movie/${result.imdbid}`} className="search-result-item-link" onClick={handleResultClick}><SearchResultItem info={result}/></Link></div>)}
+    {results.map((result, index) => <div className="row" key={`${index}-${searchTerm}`} ><Link to={`/movie/${result.imdbid}`} className="movie-preview-link" onClick={handleResultClick}><SearchResultItem info={result}/></Link></div>)}
   </React.Fragment>
 )
 
