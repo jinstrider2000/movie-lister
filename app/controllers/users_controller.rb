4d75226aab168ff6633ffcbe3238022c91dd7f66
jinstrider2000 <efrainperezjr@live.com>
2018-11-27 21:31:37 -0500

    finalized removal of all password stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-      params.permit(:username, :password)
+      params.permit(:username)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :history_items
   validates :username, presence: true
   validates :username, uniqueness: {case_sensitive: false}
