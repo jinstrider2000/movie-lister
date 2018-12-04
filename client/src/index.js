83cc1544591f92a27a642cd67362034fdc80aaa9
jinstrider2000 <efrainperezjr@live.com>
2018-12-04 00:35:08 -0500

    added redux persist basic config

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,20 @@ import App from './containers/App';
 import { Provider } from "react-redux";
 import reducer from './reducers/combinedReducer';
 import registerServiceWorker from './registerServiceWorker';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(persistedReducer,composeEnhancers(applyMiddleware(thunk)));
+const persistor = persistStore(store);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><PersistGate loading={null} persistor={persistor}><App /></PersistGate></Provider>, document.getElementById('root'));
 registerServiceWorker();
