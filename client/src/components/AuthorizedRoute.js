445e234b13019907a72966c12a7c241413907b5a
jinstrider2000 <efrainperezjr@live.com>
2018-12-04 07:22:19 -0500

    update authorizedroute comp to es6 class

--- client/src/components/AuthorizedRoute.js
+++ client/src/components/AuthorizedRoute.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {alertWarning} from '../actions/alertActions';
 import {Route, Redirect} from 'react-router-dom';
 
-const AuthorizedRoute = ({component: Component, componentProps, signedIn, alertWarning, ...routeProps}) => {
-  return (
-    <Route
-      {...routeProps}
-      render={routerProps =>
-        signedIn ? (
-          <Component {...componentProps} {...routerProps} />
-        ) : (
-          <Redirect to={{pathname: "/sign-in", state: {from: routerProps.location}}}/>
-        )
-      }
-    />
-  );
+class AuthorizedRoute extends Component {
+
+  componentDidMount() {
+    if (!this.props.signedIn) {
+      this.props.alertWarning("To view the requested page, please sign in.");
+    }
+  }
+
+  render() {
+    const {component: Component, componentProps, alertWarning, signedIn, ...routeProps} = this.props
+    return (
+      <Route
+        {...routeProps}
+        render={routerProps =>
+          signedIn ? (
+            <Component {...componentProps} {...routerProps} />
+          ) : (
+            <Redirect to={{pathname: "/sign-in", state: {from: routerProps.location}}}/>
+          )
+        }
+      />
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
@@ -23,4 +34,12 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps,null)(AuthorizedRoute);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    alertWarning: (message) => {
+      dispatch(alertWarning(message));
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(AuthorizedRoute);
